c667da145f5b6d9ff44b8de2881fc4c2501b32e6
antonin <antoninc8@gmail.com>
2021-02-03 10:57:44 +0100

    add user + connection

---
+++ api-platform-2.6.1/api/src/DataPersister/UserDataPersister.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\DataPersister;
+
+use ApiPlatform\Core\DataPersister\DataPersisterInterface;
+use App\Entity\User;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\OptimisticLockException;
+use Doctrine\ORM\ORMException;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserDataPersister implements DataPersisterInterface
+{
+    /**
+     * @var EntityManager|EntityManagerInterface
+     */
+    private EntityManager|EntityManagerInterface $entityManager;
+
+    /**
+     * @var UserPasswordEncoderInterface
+     */
+    private UserPasswordEncoderInterface $userPasswordEncoder;
+
+    /**
+     * UserDataPersister constructor.
+     * @param EntityManagerInterface $entityManager
+     * @param UserPasswordEncoderInterface $userPasswordEncoder
+     */
+    public function __construct(EntityManagerInterface $entityManager, UserPasswordEncoderInterface $userPasswordEncoder)
+    {
+        $this->entityManager = $entityManager;
+        $this->userPasswordEncoder = $userPasswordEncoder;
+    }
+
+    /**
+     * @param $data
+     * @return bool
+     */
+    public function supports($data): bool
+    {
+        return $data instanceof User;
+    }
+
+    /**
+     * @param $data
+     * @return object|void
+     * @throws ORMException
+     * @throws OptimisticLockException
+     */
+    public function persist($data)
+    {
+        if ($data->getPlainPassword()) {
+            $data->setPassword(
+                $this->userPasswordEncoder->encodePassword($data, $data->getPlainPassword())
+            );
+            $data->eraseCredentials();
+        }
+        dump("passé");
+
+        $this->entityManager->persist($data);
+        $this->entityManager->flush();
+    }
+
+    /**
+     * @param $data
+     * @throws ORMException
+     * @throws OptimisticLockException
+     */
+    public function remove($data)
+    {
+        $this->entityManager->remove($data);
+        $this->entityManager->flush();
+    }
+}
---
+++ api-platform-2.6.1/api/src/Entity/User.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Serializer\Annotation\SerializedName;
+
+/**
+ * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @ORM\Table(name="`user`")
+ */
+class User implements UserInterface
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private int $id;
+
+    /**
+     * @ORM\Column(type="string", length=180, unique=true)
+     */
+    private string $username;
+
+    /**
+     * @ORM\Column(type="json")
+     */
+    private array $roles = [];
+
+    /**
+     * @var string The hashed password
+     * @ORM\Column(type="string")
+     */
+    private string $password;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @SerializedName("password")
+     */
+    private string $plainPassword;
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUsername(): string
+    {
+        return (string) $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPlainPassword(): string
+    {
+        return $this->plainPassword;
+    }
+
+    /**
+     * @param string $plainPassword
+     */
+    public function setPlainPassword(string $plainPassword): void
+    {
+        $this->plainPassword = $plainPassword;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getPassword(): string
+    {
+        return (string) $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getSalt()
+    {
+        // not needed when using the "bcrypt" algorithm in security.yaml
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+}
